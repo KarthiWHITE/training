f01e8888551c86c918e7d581523c1356c5e86610
KarthiWHITE <karthikeyan@chargebee.com>
2015-06-17 18:21:14 +0530

    move all files to one directory

---
+++ moveFileToOneDir/src/movefiletoonedir/MoveFileToOneDir.java
@@ -0,0 +1,84 @@
+
+package movefiletoonedir;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MoveFileToOneDir {
+
+    File srcDir,destDir;
+    
+    public MoveFileToOneDir(File srcDir, File destDir){
+        
+        this.srcDir=srcDir;
+        this.destDir=destDir;
+    }
+   
+    public void moveFiles(){
+        moveFile(srcDir);
+        System.out.println(destDir.listFiles().length+ " files moved successfully");
+    }
+    
+    
+    public void moveFile(File srcDir){
+    
+        for(File file:srcDir.listFiles()){
+            
+            if(file.isDirectory()){
+                moveFile(file);
+            }else if(file.isFile() && !file.isHidden()){
+                
+                int i=1;
+                String ext=""; 
+                String fileName=file.getName();
+                while(true){
+                    
+                    if(new File(destDir.getAbsolutePath()+"/"+fileName+ext).exists()){
+                        ext="_"+i++;
+                    }else{
+                        file.renameTo(new File(destDir.getAbsolutePath()+"/"+fileName+ext));
+                        i=1;
+                        break;
+                    }
+                }
+                
+                
+            }
+            
+        }
+    
+    }
+    
+    public static void main(String[] args) throws IOException {
+        
+        File srcDir,destDir;
+        
+        System.out.println("Enter the source directory :");
+        srcDir=getDir();
+        
+        System.out.println("Enter the destination directory :");
+        
+        destDir=getDir();
+        
+        MoveFileToOneDir mov= new MoveFileToOneDir(srcDir, destDir);
+        mov.moveFiles();
+    }
+    
+    
+    public static File getDir() throws IOException{
+        File dir=new File(System.getProperty("user.home"));
+        BufferedReader in= new BufferedReader(new InputStreamReader(System.in));
+        while(true){
+            System.out.println("Enter the path from the root : "+ new File(System.getProperty("user.home")).getAbsolutePath());
+            dir=new File(System.getProperty("user.home")+in.readLine());
+            if(dir.exists()){
+                return dir;
+            }else{
+                System.out.println("The directory "+ dir.getAbsolutePath()+" is not valid");
+            }
+        }
+    }
+    
+}
